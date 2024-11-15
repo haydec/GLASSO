42b8ca3e53a425411a2fcbd531fb6bccc66aa794
chayde <cillianhayde@protonmail.com>
2024-11-15 17:44:36 +0900

    Updates to TVGL - ADMM-3

--- ADMM_TVGL_3.m
+++ ADMM_TVGL_3.m
@@ -1,39 +1,17 @@
 clear
 clc
+disp("------------START------------------")
+n_samples = 10000000;
+sections = 2;
+n = 5;
+mean_value = 0;
+std_dev = repelem(0.01,sections);
 
-sections = 10;
+[D,pre_tensor,cov_tensor] = GenerateSamples( n_samples,sections,n,mean_value,std_dev);
 
-mean_value = 0;                           % Desired mean
-std_dev = randi(20,[1,sections]);         % Desired standard deviation
-n = 5;                                    % Size of the matrix (n x n)
-
-[pre_tensor, cov_tensor] = dispersion_tensor(sections,n,mean_value,std_dev);
-
-
-
-% Number of samples to generate
-n_samples = 10000;
-data = GenerateRandomData(n,cov_tensor,n_samples,sections);
-
-plot(data)
-
-% Define the sample size and reshape data into segments
-% ============================================================================
-sampleSize = 10000;  % Define sample size (number of time steps in each segment)
-% ============================================================================
-[numObservations, numVariables] = size(data);
-numFullSamples = floor(numObservations / sampleSize);
-
-% Extract only the data that fits into full samples
-sampledData = data(1:numFullSamples * sampleSize, :);
-
-% Reshape into samples with dimensions [sampleSize, numVariables, numFullSamples]
-samples = reshape(sampledData, sampleSize, numVariables, numFullSamples);
-
-
-rho = 1;
-lambda = 0.0000; % Sparsity Penalty 
-beta = 0.1; % Time Varying Penalty
+rho = 0.01;
+lambda = 0.001; % Sparsity Penalty 
+beta = 0; % Time Varying Penalty
 
 
 alpha = 1; % alpha is the over-relaxation parameter
@@ -41,32 +19,35 @@ alpha = 1; % alpha is the over-relaxation parameter
 t_start = tic;
 %Global constants and defaults
 QUIET    = 0;
-MAX_ITER = 1000;
+MAX_ITER = 1;
 ABSTOL   = 1e-4;
 RELTOL   = 1e-2;
 
-S = zeros(size(samples,2),size(samples,2),size(samples,3));
+S = zeros(size(D,2),size(D,2),size(D,3));
 %Data preprocessing
-for t = 1:1:size(samples,3)
-    S(:,:,t) = cov(samples(:,:,t));
+for t = 1:1:size(D,3)
+    S(:,:,t) = cov(D(:,:,1)); % t = 1 !!!!!!!!!!!!!!!!!!!!!!
 end
-n = size(S,1);
-T = size(S,3);
+n = size(S,1); % Number Of Variables
+T = size(S,3); % Number Of Timesteps
 
-%ADMM solver
+% Precision Variables
 Theta_hat = zeros(size(S,1),size(S,2),size(S,3));
+
+% Consensus Variables
 Z0 = zeros(size(S,1),size(S,2),size(S,3));
 Z1 = zeros(size(S,1),size(S,2),size(S,3));
 Z2 = zeros(size(S,1),size(S,2),size(S,3));
 
+% Dual Variables
 U0 = zeros(size(S,1),size(S,2),size(S,3));
 U1 = zeros(size(S,1),size(S,2),size(S,3));
 U2 = zeros(size(S,1),size(S,2),size(S,3));
 
 
-eta = numFullSamples / (3 * rho);  % Adjusted penalty parameter for the proximal operator
+eta = n_samples / (3 * rho);  % Adjusted penalty parameter for the proximal operator
 
-BetaConsesus = (beta > 0) + (beta > 0);
+BetaConsesus = 2*(beta > 0);
 LambdaConsensus = (lambda > 0);
 NumConsensus = LambdaConsensus + BetaConsesus;
 disp("Number Of Consesus Variables: " + num2str(NumConsensus))
@@ -75,6 +56,9 @@ if NumConsensus < 1
     disp("TVGL via ADMM not defined for no constraints" )
     return
 end
+
+disp("Empirical Covariance")
+disp(S)
 
 for k = 1:1:MAX_ITER
         
@@ -86,22 +70,29 @@ for k = 1:1:MAX_ITER
     U2prev = U2; % For Diagnostics (Check Convergence)
     
     % Update Precision Matrix
-    Theta_hat = update_theta(Theta_hat,Z0,Z1,Z2,U0,U1,U2,S,eta,lambda,beta,alpha);
+    Theta_hat = update_theta(Z0,Z1,Z2,U0,U1,U2,S,eta,lambda,beta,alpha);
+    %disp("--Theta_hat--")
+    %disp(Theta_hat)
 
     % z-update with relaxation
-    Z0 =  update_z0(Theta_hat,Z0,U0,lambda,rho);    
-    [Z1,Z2] = update_z1z2(Theta_hat,Z1,Z2,U1,U2,beta,rho);
+    Z0 =  update_z0(Theta_hat,U0,lambda,rho); 
+    %disp("--Z0--")
+    %disp(Z0)
+    %[Z1,Z2] = update_z1z2(Theta_hat,Z1,Z2,U1,U2,beta,rho);
        
 
     % u-update (Dual Variable) 
     U0 = update_u0(Theta_hat,U0,Z0,lambda);
-    [U1,U2] = update_u1u2(Theta_hat,Z1,Z2,U1,U2,beta);
+    %disp("--U0--")
+    %disp(U0)
+    %[U1,U2] = update_u1u2(Theta_hat,Z1,Z2,U1,U2,beta);
 
 
   
 
-
+    
     % diagnostics, reporting, termination checks
+    %{
     history.objval(k)  = objective(S, Theta_hat, Z0,Z1,Z2, lambda,beta,T);
     
     r0 = 0;
@@ -110,9 +101,12 @@ for k = 1:1:MAX_ITER
     end
     r1 = 0;
     r2 = 0;
-    for t = 2:1:T
-        r1 = r1 + norm(Z1(:,:,t) - Z1(:,:,t-1), 'fro');
-        r2 = r2 + norm(Theta_hat(:,:,t) - Z2(:,:,t-1), 'fro');
+
+    if beta > 0
+        for t = 2:1:T
+            r1 = r1 + norm(Z1(:,:,t) - Z1(:,:,t-1), 'fro');
+            r2 = r2 + norm(Theta_hat(:,:,t) - Z2(:,:,t-1), 'fro');
+        end
     end
     
     d0 = 0;
@@ -122,9 +116,11 @@ for k = 1:1:MAX_ITER
     
     d1 = 0;
     d2 = 0;
-    for t = 2:1:T
-        d1 = d1 + norm(Z1(:,:,t) - Z1prev(:,:,t-1), 'fro');
-        d2 = d2 + norm(Z2(:,:,t) - Z2prev(:,:,t-1), 'fro');
+    if beta > 0
+        for t = 2:1:T
+            d1 = d1 + norm(Z1(:,:,t) - Z1prev(:,:,t-1), 'fro');
+            d2 = d2 + norm(Z2(:,:,t) - Z2prev(:,:,t-1), 'fro');
+        end
     end
 
 
@@ -146,37 +142,31 @@ for k = 1:1:MAX_ITER
          break;
     end
  
-
+%}
 
 end
 fprintf('iter\t primal_norm\t primal_tol\t dual_norm\t dual_tol\t Obj\n')
-disp(" ")
+disp("THETA HAT 1st TimeStep")
 disp(Theta_hat(:,:,1))
+disp("Pre Tensor 1st TimeStep")
 disp(pre_tensor(:,:,1))
 
-function Theta_hat = update_theta(Theta_hat,Z0,Z1,Z2,U0,U1,U2,S,eta,lambda,beta,alpha)
-        
+function Theta_hat = update_theta(Z0,Z1,Z2,U0,U1,U2,S,eta,lambda,beta,alpha)
+
+        Theta_hat = zeros(size(S));
         T = size(S,3);
         for t = 1:1:T
-            
-            if t == 1 
-               A = ( ( Z0(:,:,t) + Z1(:,:,t)) - ( U0(:,:,t) + U2(:,:,t) ) )/( (lambda>0) + (beta>0) );
-
-            elseif t == T
-               A = ( ( Z0(:,:,t) + Z2(:,:,t)) - ( U0(:,:,t) + U1(:,:,t) ) )/( (lambda>0) + (beta>0) );
-            else
-               A = ( ( Z0(:,:,t) + Z1(:,:,t) +  Z2(:,:,t)) - (U0(:,:,t) + U1(:,:,t) +  U2(:,:,t)) )/( (lambda>0) + (beta>0) + (beta>0) );
-            end
-
-
+            disp("------------------------------")
+            A = ( ( Z0(:,:,t) + Z1(:,:,t) +  Z2(:,:,t) ) - ( U0(:,:,t) + U1(:,:,t) +  U2(:,:,t) ) )/( (lambda>0) + 2*(beta>0)  );
+        
             % Theta Update
             
-            Asym = (A + A')/2;
-            M = (1/eta)*(Asym) - S(:,:,t); % (rho)*(Asym) - S;
-            [Q,L] = eig(M);
-            es = diag(L);
-            xi = (es + sqrt( es.^2 + 4*(1/eta) )) ./( 2*( 1/eta )); % (es + sqrt(es.^2 + 4*rho))./(2*rho);
-            Theta =  Q*diag(xi)*Q';
+            Asym = (A + A')/2
+            M = (1/eta)*(Asym) - S(:,:,t) % (rho)*(Asym) - S;
+            [Q,L] = eigs(M)
+            es = diag(L)
+            xi = (es + sqrt( es.^2 + 4*(1/eta) )) ./( 2*( 1/eta )) % (es + sqrt(es.^2 + 4*rho))./(2*rho);
+            Theta =  Q*diag(xi)*Q'
     
     
             
@@ -187,17 +177,32 @@ function Theta_hat = update_theta(Theta_hat,Z0,Z1,Z2,U0,U1,U2,S,eta,lambda,beta,
 end
 
 
-function Z0 =  update_z0(Theta_hat,Z0,U0,lambda,rho)
+function Z0 =  update_z0(Theta_hat,U0,lambda,rho)
 
+        Z0 = zeros(size(Theta_hat));
         if lambda > 0
             T = size(Theta_hat,3);
             for t = 1:1:T
-                Z0(:,:,t) = soft_threshold_odd(Theta_hat(:,:,t) + U0(:,:,t), lambda,rho);
+                Z0(:,:,t) = soft_threshold_odd(Theta_hat(:,:,t) + U0(:,:,t), lambda, rho);
             end
         end
 
 
 end
+
+
+function U0 = update_u0(Theta_hat,U0,Z0,lambda)
+        
+        
+        if lambda > 0
+            T = size(Theta_hat,3);
+            for t = 1:1:T
+                U0(:,:,t) = U0(:,:,t) + ( Theta_hat(:,:,t) - Z0(:,:,t) );
+            end
+        end
+    
+end
+
 
 function [Z1,Z2] = update_z1z2(Theta_hat,Z1,Z2,U1,U2,beta,rho)
         
@@ -217,16 +222,7 @@ function [Z1,Z2] = update_z1z2(Theta_hat,Z1,Z2,U1,U2,beta,rho)
 
 end
 
-function U0 = update_u0(Theta_hat,U0,Z0,lambda)
-    
-        if lambda > 0
-            T = size(Theta_hat,3);
-            for t = 1:1:T
-                U0(:,:,t) = U0(:,:,t) + ( Theta_hat(:,:,t) - Z0(:,:,t) );
-            end
-        end
-    
-end
+
 
 function [U1,U2] = update_u1u2(Theta_hat,Z1,Z2,U1,U2,beta)
 
@@ -328,57 +324,3 @@ function E = L1_element_wise(A, beta, rho)
 end
 
 
-
-
-function [pre_matrix, cov_matrix] = dispersion_matrix(n,mean_value,std_dev)
-    % Generate random numbers from the specified normal distribution
-    matrix = mean_value + std_dev * randn(n, n);
-    
-    % Construct the positive definite covariance matrix
-    cov_matrix = matrix' * matrix;
-    
-    % Scale the matrix to have variances close to 1 on the diagonal (optional)
-    cov_matrix = cov_matrix ./ max(abs(diag(cov_matrix)));
-    isPositiveDef(cov_matrix)
-    
-    % precision matrix
-    pre_matrix = inv(cov_matrix);
-    isPositiveDef(pre_matrix)
-end
-
-function [pre_tensor, cov_tensor] = dispersion_tensor(sections,n,mean_value,std_dev)
-    
-    pre_tensor = zeros(n,n,sections);
-    cov_tensor = zeros(n,n,sections);
-    
-    for s = 1:1:sections
-        [pre_matrix, cov_matrix] = dispersion_matrix(n,mean_value,std_dev(s));
-        pre_tensor(:,:,s) = pre_matrix;
-        cov_tensor(:,:,s) = cov_matrix;
-    end
-
-end
-
-
-function data = GenerateRandomData(num_variables,true_covariance,n_samples,sections)
-    
-    data = zeros(n_samples*sections,num_variables);
-    Start = 1;
-    End = n_samples;
-    for s = 1:1:sections
-    
-        rng(0);  % Set random seed for reproducibility
-        data(Start:End,:) = mvnrnd(zeros(1, num_variables), true_covariance(:,:,s), n_samples);
-        Start = Start + n_samples;
-        End = End + n_samples;
-    end
-
-end
-
-function isPositiveDef(X)
-    % Check that true_precision is positive definite
-    eigvals = eig(X);
-    if any(eigvals <= 0)
-        disp('matrix is not positive definite');
-    end
-end
